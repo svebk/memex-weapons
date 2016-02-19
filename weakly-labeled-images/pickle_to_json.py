e0529ad889399609aadd20a0ca7b440c39efd6ca
Svebor <svebor.karaman@gmail.com>
2016-02-19 18:28:54 -0500

    proper json

--- weakly-labeled-images/pickle_to_json.py
+++ weakly-labeled-images/pickle_to_json.py
@@ -5,4 +5,4 @@ ga_data=pickle.load(open('guns_america.pickle','rb'))
 with open('guns_america.json','wt') as f: 
   for key in ga_data.keys():
     print "Saving doc",key
-    f.write('{'+key+','+json.dumps(ga_data[key])+'}\n')
+    f.write('{"'+key+'":'+json.dumps(ga_data[key])+'}\n')
