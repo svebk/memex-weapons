26f78d630f408a3c24d834d7859ba0a1ce6a8145
Svebor <svebor.karaman@gmail.com>
2016-02-26 18:25:36 -0500

    weapon classes

--- deepmodels/pycaffe_extractor.py
+++ deepmodels/pycaffe_extractor.py
@@ -12,81 +12,93 @@ import caffe
 import cv2
 import scipy
 
-def show_res_batch(out,cat_lists,batch_in):
-	prediction = out['prob']
-	np_cat=np.asarray(cat_lists)
-	for i in range(batch_in):
-		print "Input #"+str(i)+" classified as:",np_cat[prediction[i].argmax()] 
-		ind=np.argsort(prediction[i])[::-1]
-		print "Top 10 classes are:",np_cat[ind[0:10]]	
+class CaffeExtractorConf():
+    MODEL_FILE = './deepmodels/CNN_20K_deploy.prototxt'
+    PRETRAINED = './deepmodels/CNN_20K.caffemodel'
+    MEAN_FILE = './deepmodels/imagenet_mean.npy'
+    CAT_FILE = './deepmodels/cat_20K.txt'
+    WEAPONS_CAT_FILE = './deepmodels/weapon_classes.npy"
+    WITH_FLIP = True
+    WITH_CROP = False
+    OUTPUT_LAYERS=['prob']
+    CAFFE_MODE = "CPU"
+
+class CaffeExtractor():
+    conf = None  
+    net = None  
+    mean_img = None
+
+    def __init__(self, in_conf=CaffeExtractorConf()):
+	self.conf = in_conf
+	self.initialize()
+
+    def initialize(self):
+        if self.conf.CAFFE_MODE == "GPU":
+              caffe.set_mode_gpu()
+        else:
+	      caffe.set_mode_cpu()
+	self.net = caffe.Net(self.conf.MODEL_FILE, self.conf.PRETRAINED, caffe.TEST)
+	self.mean_img = np.load(self.conf.MEAN_FILE)
+	self.np_cat = self.loadCatList(self.conf.CAT_FILE)
+	self.conf.batch_size = self.net.blobs['data'].data.shape[0]
+	self.conf.IN_DIM = (self.net.blobs['data'].data.shape[2],self.net.blobs['data'].data.shape[3])
+	self.wf=np.where(np.load(self.conf.WEAPONS_CAT_FILE)==1)[0]
+
+    def formatInput(self,IMAGE_FILE):
+	start=(256-224)/2
+	end=start+224
+        input_image = cv2.imread(IMAGE_FILE)
+        input_image_t=np.transpose(input_image,(2,0,1))
+        if self.conf.WITH_FLIP:
+            input_image_fl = np.fliplr(input_image)
+            input_image_fl_t=np.transpose(input_image,(2,0,1))
+            if self.conf.WITH_CROP:
+                input_data = np.asarray([(input_image_t[:,start:end,start:end]-self.mean_img[:,start:end,start:end]),(input_image_fl_t[:,start:end,start:end]-self.mean_img[:,start:end,start:end])])
+            else:
+                input_image_tr=scipy.misc.imresize(input_image_t.transpose(1,2,0),self.conf.IN_DIM).transpose(2,0,1)
+        	input_image_fl_tr=scipy.misc.imresize(input_image_fl_t.transpose(1,2,0),self.conf.IN_DIM).transpose(2,0,1)
+                input_data = np.asarray([(input_image_tr-self.mean_img[:,start:end,start:end]),(input_image_fl_tr-self.mean_img[:,start:end,start:end])])
+        else:
+            if self.conf.WITH_CROP:
+                input_data = np.asarray([(input_image_t[:,start:end,start:end]-self.mean_img[:,start:end,start:end])])
+            else:
+                input_image_t=scipy.misc.imresize(input_image_t.transpose(1,2,0),self.conf.IN_DIM).transpose(2,0,1)
+                input_data = np.asarray([(input_image_t-self.mean_img[:,start:end,start:end])])
+	return input_data
+
+    def getOutput(self,IMAGE_FILE):
+	self.input_data = None
+        self.input_data = self.formatInput(IMAGE_FILE)
+	self.net.forward(data=self.input_data)
+	self.out = {}
+	for layer in self.conf.OUTPUT_LAYERS:
+		self.out[layer]=self.net.blobs[layer].data
+	return self.out
+
+    @staticmethod
+    def loadCatList(CAT_FILE):
+        cat_lists=[]
+        with open(CAT_FILE,"rt") as fcat:
+            for line in fcat:
+                cat_lists.append(line)
+	print "We have",str(len(cat_lists)),"classes in total."
+	return np.asarray(cat_lists)
+
+    def show_res_batch(self):     
+	batch_in = self.input_data.shape[0]
+	prediction = self.out['prob']
+        for i in range(batch_in):
+            print "Input #"+str(i)+" classified as:",self.np_cat[prediction[i].argmax()] 
+            ind=np.argsort(prediction[i])[::-1]
+            print "Top 10 classes are:",self.np_cat[ind[0:10]]	
+	    mlw=self.wf[np.argmax(prediction[i][self.wf])]
+	    print "Most likely weapon is:",self.np_cat[mlw].strip()
 
 
-def show_res(out,cat_lists,WITH_FLIP):
-	prediction = out['prob']
-	np_cat=np.asarray(cat_lists)
-	if WITH_FLIP:
-		ind=np.argsort(prediction)[:,::-1]
-		print np_cat[ind[0][0:10]]
-		print np_cat[ind[1][0:10]]
-		print 'predicted class:', np_cat[prediction[0].argmax()], prediction[0].argmax()
-		print 'predicted class:', np_cat[prediction[1].argmax()], prediction[1].argmax()	
-	else:
-		ind=np.argsort(prediction)[0,::-1]
-		print np_cat[ind[0:10]]
-		print 'predicted class:', np_cat[prediction.argmax()], prediction.argmax()	
 
-
-# Set the right path to your model definition file, pretrained model weights,
-# and the image you would like to classify.
-MODEL_FILE_FLIP = './deepmodels/CNN_20K_deploy.prototxt'
-MODEL_FILE = './deepmodels/CNN_20K_deploy_noflip.prototxt'
-MODEL_FILE_BATCH32 = './deepmodels/CNN_20K_deploy_batch32.prototxt'
-PRETRAINED = './deepmodels/CNN_20K.caffemodel'
-MEAN_FILE = './deepmodels/imagenet_mean.npy'
-CAT_FILE = './deepmodels/cat_20K.txt'
-WITH_FLIP = True
-WITH_CROP = False
-IN_DIM=(224,224)
-
-cat_lists=[]
-with open(CAT_FILE,"rt") as fcat:
-  for line in fcat:
-    cat_lists.append(line)
-print len(cat_lists)
-
+ce = CaffeExtractor()
 IMAGE_FILE = '/media/04/MEMEX/memex-weapons/deepmodels/resized/04_Marlin_Modelo_1894.jpg'
-caffe.set_mode_cpu()
-
-if WITH_FLIP:
-	net = caffe.Net(MODEL_FILE_FLIP, PRETRAINED, caffe.TEST)
-else:
-	net = caffe.Net(MODEL_FILE_BATCH32, PRETRAINED, caffe.TEST)
-
-start=(256-224)/2
-end=start+224
-
-mean_img = np.load(MEAN_FILE)
-input_image = cv2.imread(IMAGE_FILE)
-input_image_t=np.transpose(input_image,(2,0,1))
-if WITH_FLIP:
-    input_image_fl = np.fliplr(input_image)
-    input_image_fl_t=np.transpose(input_image,(2,0,1))
-    if WITH_CROP:
-        input_data = np.asarray([(input_image_t[:,start:end,start:end]-mean_img[:,start:end,start:end]),(input_image_fl_t[:,start:end,start:end]-mean_img[:,start:end,start:end])])
-    else:
-        input_image_tr=scipy.misc.imresize(input_image_t.transpose(1,2,0),IN_DIM).transpose(2,0,1)
-	input_image_fl_tr=scipy.misc.imresize(input_image_fl_t.transpose(1,2,0),IN_DIM).transpose(2,0,1)
-        input_data = np.asarray([(input_image_tr-mean_img[:,start:end,start:end]),(input_image_fl_tr-mean_img[:,start:end,start:end])])
-else:
-    if WITH_CROP:
-        input_data = np.asarray([(input_image_t[:,start:end,start:end]-mean_img[:,start:end,start:end])])
-    else:
-        input_image_t=scipy.misc.imresize(input_image_t,IN_DIM)
-        input_data = np.asarray([(input_image_t-mean_img[:,start:end,start:end])])
-
-net.outputs
-out = net.forward_all(data=input_data)
-#show_res(out,cat_lists,WITH_FLIP)
-show_res_batch(out,cat_lists,input_data.shape[0])
+out = ce.getOutput(IMAGE_FILE)
+ce.show_res_batch()
 
 
